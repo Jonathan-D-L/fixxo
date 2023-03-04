fd7b148b43c20fadecb8c6de065e620163947a6d
Jonathan.Donath.Lopez <Jonathan.Donath.Lopez@hotmail.com>
2023-03-04 15:22:21 +0100

    fixxo

--- js/script.js
+++ js/script.js
@@ -37,11 +37,11 @@ function convertToStars(starRating) {
 async function getData() {
     const res = await fetch('https://kyh-net22.azurewebsites.net/api/products')
     const data = await res.json()
-    for (let product of data) {
-        console.log(product)
-        if (product.tag === 'featured') {
-            document.getElementById('products').innerHTML +=
-                `<div class="product-card">
+    try {
+        for (let product of data) {
+            if (product.tag === 'featured') {
+                document.getElementById('products').innerHTML +=
+                    `<div class="product-card">
                 <div class="product-card-img">
                 <img src="${product.imageUrl}">
                 <div class="product-card-menu">
@@ -62,30 +62,132 @@ async function getData() {
                 <p class="product-card-price">${product.originalPrice} ${product.currency}</p>
                 </div>
         </div>`
+            }
         }
+    }
+    catch {
+
     }
 }
 getData()
 
+
+
 //apiname/swagger
 
-async function handleContactForm(e) {
-    e.preventDefault()
 
-    const form = {
-        name: "",
-        email: "",
-        comments: ""
+
+function validateEmail(element) {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const errorElement = document.getElementById(`error-${element.name}`)
+
+    if (!regEx.test(element.value)) {
+        errorElement.innerHTML = `You must provide an valid e-mail address.`
+        return false
     }
+    errorElement.innerHTML = ``
+    return true
+}
 
-    const res = await fetch(`https://kyh-net22.azurewebsites.net/api/contacts`, {
-        method: 'post',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(form)
-    })
-    if (res.status === 200) {
-        console.log('Tack för din förfrågan!')
+function validateName(element) {
+    const regEx = /^([a-öA-Ö\u00C0-\u017F]+(([' -][a-öA-Ö])?[a-öA-Ö]*)){2,}$/
+    const errorElement = document.getElementById(`error-${element.name}`)
+    if (!regEx.test(element.value)) {
+        errorElement.innerHTML = `You must provide a valid name.`
+        return false
+    }
+    errorElement.innerHTML = ``
+    return true
+}
+
+function validateComment(element) {
+    const errorElement = document.getElementById(`error-${element.name}`)
+    if (element.value.trim() === '') {
+        errorElement.innerHTML = `You must provide a comment.`
+        return false
+    }
+    errorElement.innerHTML = ``
+    return true
+}
+
+function validate(event) {
+    switch (event.target.name) {
+        case 'email':
+            validateEmail(event.target)
+            break;
+        case 'name':
+            validateName(event.target)
+            break;
+        case 'comment':
+            validateComment(event.target)
+            break;
     }
 }
+
+async function handleSubmit(e) {
+    e.preventDefault()
+    const errors = []
+    const errorMessage = document.getElementById('error-submit')
+    errorMessage.innerHTML = ''
+
+    for (let element of e.target) {
+        if (element.required) {
+            const errorElement = document.getElementById(`error-${element.name}`)
+            if (element.value.length === 0) {
+                switch (element.name) {
+                    case 'email':
+                        errorElement.innerHTML = `You must provide an valid ${element.name} adress.`
+                        break
+                    case 'name':
+                        errorElement.innerHTML = `You must provide a valid ${element.name}.`
+                        break
+                    case 'comment':
+                        errorElement.innerHTML = `You must provide a ${element.name}.`
+                        break
+                }
+                errors.push(false)
+            } else {
+                errorElement.innerHTML = ``
+
+                switch (element.name) {
+                    case 'email':
+                        errors.push(validateEmail(element))
+                        break
+                    case 'name':
+                        errors.push(validateName(element))
+                        break
+                    case 'comment':
+                        errors.push(validateComment(element))
+                        break
+                }
+            }
+        }
+    }
+}
+
+
+
+// async function handleContactForm(e) {
+//     e.preventDefault()
+
+//     const form = {
+//         name: "",
+//         email: "",
+//         comments: ""
+//     }
+
+//     const res = await fetch(/*`https://kyh-net22.azurewebsites.net/api/contacts`*/'', {
+//         method: 'post',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(form)
+//     })
+
+//     if (res.status === 200) {
+//         console.log('Tack för din förfrågan!')
+//     }
+//     else{
+//         console.log('error')
+//     }
+// }
